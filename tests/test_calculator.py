00b89a939a35eb16a29548a035fe5453b96358be
Makshota12 <Vip241202@yandex.ru>
2025-11-07 12:51:54 +0300

    Add multiply function

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,10 @@
-from calculator import add, subtract
+from calculator import add, subtract, multiply
 
 def test_add():
     assert add(2, 2) == 4
 
 def test_subtract():
     assert subtract(5, 3) == 2
-    
+
+def test_multiply():
+    assert multiply(3, 4) == 12
